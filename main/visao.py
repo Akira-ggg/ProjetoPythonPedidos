ee67ee43ecd4d434040decff00875be9d011244f
Akira-ggg <akiraartur100@gmail.com>
2026-07-09 20:46:56 -0300

    Atualização

--- main/visao.py
+++ main/visao.py
@@ -1,12 +1,19 @@
 from models.pedidos import Pedidos
 from repository.pedido_repository import repository
-nome = input("Qual o nome do produto: ")
-print()
-quantidade = int(input("Digite a quantidade"))
-print()
-preco = float(input("Qual o preco "))
 
-pedido = Pedidos(nome,quantidade,preco)
-rp = repository()
-rp.adicionarSql(pedido)
+def main():
+    nome = input("Qual o nome do produto: ")
+    print()
 
+    quantidade = int(input("Digite a quantidade: "))
+    print()
+
+    preco = float(input("Qual o preço: "))
+
+    pedido = Pedidos(nome, quantidade, preco)
+
+    rp = repository()
+    rp.adicionarSql(pedido)
+
+if __name__ == "__main__":
+    main()
